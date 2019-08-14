ca5945fecec0344b8bfbe8274dfad623ffe86fed
Hien <thuhien.bk0301@gmail.com>
2019-08-14 21:07:42 +0700

    Lesson 1

---
+++ JavaApplication1/src/javaapplication1/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PV
+ */
+public class NewClass {
+    public static void main (String[]args){
+        int a=7;
+        float b=3;
+  
+        System.out.println(a%b);
+        
+    }
+    
+}
---
+++ JavaApplication1/src/javaapplication1/lesson1.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author PV
+ */
+public class lesson1 {
+    public static void main(String[]args){
+        //Variables
+        //Declare
+        //int a=3;//01001
+        int bin=0b10;//=3 binary
+        int hex=0xabcdef;//hexa
+        //int oct=082312;
+        char c='a';//'c'
+        double d=3.4;//
+        float f=4.56f;
+        boolean t=true;
+        String S= "hien";
+        //
+      // System.out.print("Hien xinh gai");//cout<<"Hien xinh gai";
+        // Giai phuong trinh bac nhat ax+b=0;
+        //float a=7;
+        float b=3;
+        float x;
+       // x=-b/a;
+        //+-*/%
+        // / %
+        // int/float => float
+        // int/int =>chia lay phan nguyen
+         int sodu=7%3;
+       // System.out.println(sodu);
+        // sc=new Scanner(System.in);
+        int a;
+       // String input;
+        //System.out.println("Nhap vao :");
+      //  a=sc.nextInt();//Nhap vao so nguyen
+     // input =sc.nextLine();
+        //System.out.println("Gia tri a vua nhap vao la: "+input);//sout-
+        
+        //ax+b=0;
+        int q=3;
+        float p=5.556f;
+        String w="thang";
+        String o="OOP";
+        System.out.printf("p= %d \nq = %.2f\n",q,p);
+      
+               
+        
+              
+        
+    }
+    
+}
